2d315d7947b6ed039b562e97c4b308c9598c8acd
Sankash Thakuria <sankash.thakuria@gmail.com>
2019-05-21 12:32:40 +0800

    Added multithreading

--- src/main/java/com/zendesk/search/App.java
+++ src/main/java/com/zendesk/search/App.java
@@ -2,11 +2,17 @@ package com.zendesk.search;
 
 import com.zendesk.search.console.Menu;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by @author Sankash on 5/15/2019
  */
 public class App {
     public static void main(String[] args){
-        Menu.searchLauncher();
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Runnable task = Menu::searchLauncher;
+        executorService.execute(task);
+        executorService.shutdown();
     }
 }
